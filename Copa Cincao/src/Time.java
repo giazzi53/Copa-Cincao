23a66541e9808c12749582c32307f4264023a219
Guilherme Giazzi <32527872+giazzi53@users.noreply.github.com>
2018-06-28 14:38:04 -0300

    Add files via upload

---
+++ Copa Cincao/src/Time.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GuilhermeSilvestreGi
+ */
+public class Time {
+    private String nomeTime;
+    
+    public Time(String nomeTime){
+        this.nomeTime = nomeTime;
+    }
+
+    public String getNomeTime() {
+        return nomeTime;
+    }
+
+    public void setNomeTime(String nomeTime) {
+        this.nomeTime = nomeTime;
+    }
+    
+    
+}
